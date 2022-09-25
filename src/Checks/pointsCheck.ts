4c29b9645db7f4497dbc79889cd6f989b34c55f4
Pocky <79219650+PockySweet@users.noreply.github.com>
2022-09-25 17:19:42 +0200

    Update pointsCheck.ts
    Added the autopoints flag check :)

--- src/Checks/pointsCheck.ts
+++ src/Checks/pointsCheck.ts
@@ -19,7 +19,7 @@ export async function pointsCheck(channelLogin: string) {
 }
 
 
-async function checkisClaimeable(request:any, channelId: string) {
+async function checkisClaimeable(request:any, channelId: string, autopoints: boolean) {
     let ClaimId = '';
     try {
         ClaimId = request[0].data.community.channel.self.communityPoints.availableClaim.id
@@ -35,10 +35,12 @@ async function checkisClaimeable(request:any, channelId: string) {
                 claimID: ClaimId
             }
         }
-        const claimrequest = await TwitchGQL._SendQuery("ClaimCommunityPoints", opts, '46aaeebe02c99afdf4fc97c7c0cba964124bf6b0af229395f1f6d1feed05b3d0', 'OAuth ' + userdata.auth_token, true, {}, true);
-        points = claimrequest[0].data.claimCommunityPoints.currentPoints
-        winston.info(chalk.gray('Claimed Channel Points...'), {event: "claim"})
+		 if (autopoints) {
+            //Auto Points if possible
+					const claimrequest = await TwitchGQL._SendQuery("ClaimCommunityPoints", opts, '46aaeebe02c99afdf4fc97c7c0cba964124bf6b0af229395f1f6d1feed05b3d0', 'OAuth ' + userdata.auth_token, true, {}, true);
+					if (autopoints) points = claimrequest[0].data.claimCommunityPoints.currentPoints
+                    winston.info(chalk.gray('Claimed Channel Points...'), {event: "claim"})
+					if (!autoclaim) winston.info(chalk.gray('Not Claiming Points...'), {event: "claim"})
+        }
     }
 }
-
-
